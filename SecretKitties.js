d83b8f95c068e81cc34f175e1337c12495a4f959
Clément V <clem.vercollier@gmail.com>
2021-11-05 00:14:58 +0100

    Update SecretKitties.js

--- SecretKitties.js
+++ SecretKitties.js
@@ -2,6 +2,7 @@
 // @name           SecretKitty
 // @description    Un utilitaire pour faire parler les chats
 // @author         kateblolo
+// @namespace      faemoinzmvoiqnervmoiqernvominqervmoiqnervmoinqerv
 // @version        0.0.1
 // @match          https://www.jeuxvideo.com/forums/*
 // @match          https://m.jeuxvideo.com/forums/*
